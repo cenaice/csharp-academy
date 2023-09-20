3e0d6b9d58ff0a7439afac2006bf5840acbb6c27
Victor <victorphiathep@gmail.com>
2023-09-20 17:10:16 -0400

    Level 6 completed

---
+++ MicrosoftLearn/loops-projects/Program.cs
@@ -0,0 +1,22 @@
+﻿// Code project 1 - write code that validates integer input
+
+string? readResult;
+int val = 0;
+int val_2 = 0;
+Console.WriteLine("Enter an integer value between 5 and 10");
+val = int.TryParse(readResult, out val);
+
+while (val > 10 || val < 5) {
+    Console.WriteLine("Sorry, you entered an invalid number. Please try again.");
+    valid = int.TryParse(readResult, out val);
+}
+
+Console.WriteLine($"You entered {val}. Please enter a numbeer between 5 and 10.");
+    val_2 = int.TryParse(readResult, out val_2);
+
+while (val_2 > 10 || val_2 < 5) {
+    Console.WriteLine("Sorry, you entered an invalid number. Please try again.");
+    val_2 = int.TryParse(readResult, out val_2);
+}
+
+Console.ReadLine($"Your input value ({val_2}) has been accepted.");
