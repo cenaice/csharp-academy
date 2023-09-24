b65beabb876450fc3d7e4a0901ce87ede5593ac2
Victor <victorphiathep@gmail.com>
2023-09-23 20:48:30 -0400

    Level 16 Enumerations

--- LVL6_C#-TypeSystem/Program.cs
+++ LVL6_C#-TypeSystem/Program.cs
@@ -1,16 +1,17 @@
-﻿// To properly convert input read in from user
+﻿//// To properly convert input read in from user
 
-Console.Write("What is your favorite number?");
-var input = Convert.ToInt32(Console.ReadLine());
+//Console.Write("What is your favorite number?");
+//var input = Convert.ToInt32(Console.ReadLine());
 
-// We can always use Int64 for LARGE numbers but that is unnecessary
+//// We can always use Int64 for LARGE numbers but that is unnecessary
 
-int[] arr = { input };
-Console.Write(input);
-Console.WriteLine(arr[0]);
+//int[] arr = { input };
+//Console.Write(input);
+//Console.WriteLine(arr[0]);
 
-// Alternatively we can use Parse
-Console.Write("What is your least favorite number?");
-var newInput = int.Parse(Console.ReadLine());
-int number = int.Parse("9000")
+//// Alternatively we can use Parse
+//Console.Write("What is your least favorite number?");
+//var newInput = int.Parse(Console.ReadLine());
+//int number = int.Parse("9000")
+
 
